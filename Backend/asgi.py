16f984242633e902430a6022188bb940a7bb4feb
ankitsharma97 <ankit972125@gmail.com>
2024-06-18 12:18:57 +0530

    callSet

--- Backend/asgi.py
+++ Backend/asgi.py
@@ -1,23 +1,24 @@
-# Backend/asgi.py
-
 import os
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
-from chat.channels_middleware import TokenAuthMiddlewareStack
-import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.settings')
+django.setup()  # Initialize Django
+
+# Import after setting up Django
+from chat.channels_middleware import TokenAuthMiddlewareStack
+import chat.routing
 
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": TokenAuthMiddlewareStack(
-        AuthMiddlewareStack(
+       AuthMiddlewareStack( 
             URLRouter(
-                chat.routing.websocket_urlpatterns
-            )
-        )
+            chat.routing.websocket_urlpatterns
+        ))
     ),
 })
